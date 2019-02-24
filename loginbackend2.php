dbcbda88fda24c1bc19e47aa93249981118a1a22
nand kishor <37684514+dxkissor@users.noreply.github.com>
2019-02-24 11:29:20 +0530

    Update loginbackend2.php

--- loginbackend2.php
+++ loginbackend2.php
@@ -1,12 +1,10 @@
 <?php
 $u =$_POST['username'];
 $p =$_POST['password'];
-$e =$_POST['email'];
-$pho =$_POST['phoneno'];
 
 $con=mysqli_connect('localhost','biryani','biryani');
    mysqli_select_db($con,'biryani');
-   $query="select * from usertable where  email='$e' and password='$p'";
+   $query="select * from usertable where username='$u' and password='$p'";
    $r=mysqli_query($con,$query);
    $n= mysqli_num_rows($r);
    if($n==1)
